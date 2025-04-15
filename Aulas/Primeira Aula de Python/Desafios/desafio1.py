c57b9a0db727483bd723f75127f1cbae5dabfb24
Lucas Ximenes <82716172+ximenesl@users.noreply.github.com>
2025-04-14 21:16:56 -0300

    Add files via upload

---
+++ Aulas/Primeira Aula de Python/Desafios/desafio1.py
@@ -0,0 +1,14 @@
+"""
+Desafio 1: Conversa sobre café
+Crie variáveis para armazenar:
+O nome dela
+Sua idade
+O nome de sua bebida favorita no café da manhã
+Imprima uma frase que a apresente usando as informações que você armazenou.
+"""
+
+nome = 'Gabriel'
+idade = 29
+bebida = 'Vinho'
+
+print('Este é o {}, ele tem {} anos e adora tomar {} pela manhã (faz bem ao coração[comprovadamente]).'.format(nome, idade, bebida))
